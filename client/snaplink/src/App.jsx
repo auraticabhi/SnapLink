d1ef6e2d52365ec74467710c3c7f94d12c81bbde
Abhijeet Gupta <abhijeetgupta989@gmail.com>
2023-12-22 20:51:34 +0530

    Update App.jsx

--- client/snaplink/src/App.jsx
+++ client/snaplink/src/App.jsx
@@ -10,56 +10,76 @@ import axios from 'axios'
 import Loading from './components/Loading'
 
 function App() {
-
-    const location = useLocation();
-    //console.log("logindt", localStorage.getItem("logindata"))
-
-    const user = useSelector((state) => state.user);
-    const userId = localStorage.getItem('logindata')
+  const location = useLocation();
+  const user = useSelector((state) => state.user);
+  const userId = localStorage.getItem('logindata')
     ? JSON.parse(localStorage.getItem('logindata')).user._id
     : null;
-    const token = useSelector((state) => state.token);
-    let [userr, setUserr] = useState(false);
-    const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
-    async function getUser() {
-    try{
-    const data = await axios.get(`https://snaplink-backend.onrender.com/users/${userId}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    //console.log(data.data);
-    dispatch(setUserx({ user: data.data }));
-    //console.log('heyyy');
-    setUserr(true);
-    }
-    catch(err){
+  async function getUser() {
+    try {
+      const { data } = await axios.get(`https://snaplink-backend.onrender.com/users/${userId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      dispatch(setUserx({ user: data }));
+    } catch (err) {
       toast.error("Something went wrong");
       console.log(err.message);
-      }
+    } finally {
+      setLoading(false);
     }
+  }
 
-    useEffect(() => {
-    if (userId && user == null) {
+  useEffect(() => {
+    if (userId && user === null) {
       getUser();
-    } else if (user != null) {
-      setUserr(true);
+    } else {
+      setLoading(false);
     }
-    }, []);
-    
-    const isLogin = useSelector((state) => {
-        if (state.token) return true
-        else return false;
-    })
+  }, [userId, user]);
 
-    return (
-        <div className = "app">
+  const isLogin = useSelector((state) => !!state.token);
+
+  return (
+    <div>
+      <div className="app">
         <Routes>
-        <Route path = "/" element = { < LoginPage / > }/> 
-        <Route path = "/home" element = { isLogin ? ( userr?< HomePage / >:<Loading/> ) : ( < Navigate to = "/" / > ) }/> 
-        <Route path = "/profile/:userId" element = { isLogin ? ( userr?< ProfilePage key = { location.pathname } />:<Loading/> ):<Navigate to="/" />}/> 
-        </Routes> 
-        </div>                                          
-        )
-    }
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/home"
+            element={
+              isLogin ? (
+                loading ? (
+                  <Loading />
+                ) : (
+                  <HomePage />
+                )
+              ) : (
+                <Navigate to="/" />
+              )
+            }
+          />
+          <Route
+            path="/profile/:userId"
+            element={
+              isLogin ? (
+                loading ? (
+                  <Loading />
+                ) : (
+                  <ProfilePage key={location.pathname} />
+                )
+              ) : (
+                <Navigate to="/" />
+              )
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
+}
 
-    export default App;
+export default App;
