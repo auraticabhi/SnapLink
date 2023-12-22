48ae94c44a2c3f97f2f816a6b86450550be86a63
Abhijeet Gupta <abhijeetgupta989@gmail.com>
2023-12-22 13:01:45 +0530

    Update App.jsx

--- client/snaplink/src/App.jsx
+++ client/snaplink/src/App.jsx
@@ -7,13 +7,51 @@ import { useSelector } from 'react-redux'
 function App() {
 
     const location = useLocation();
-    console.log("logindt", localStorage.getItem("logindata"))
+    //console.log("logindt", localStorage.getItem("logindata"))
+
+    const user = useSelector((state) => state.user);
+    const userId = localStorage.getItem('logindata')
+    ? JSON.parse(localStorage.getItem('logindata')).user._id
+    : null;
+    const token = useSelector((state) => state.token);
+    let [userr, setUserr] = useState(false);
+    const dispatch = useDispatch();
+
+    async function getUser() {
+    try{
+    const data = await axios.get(`http://localhost:3001/users/${userId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    //console.log(data.data);
+    dispatch(setUserx({ user: data.data }));
+    //console.log('heyyy');
+    setUserr(true);
+    }
+    catch(err){
+      toast.error("Something went wrong");
+      console.log(err.message);
+      }
+    }
+
+    useEffect(() => {
+    if (userId && user == null) {
+      getUser();
+    } else if (user != null) {
+      setUserr(true);
+    }
+    }, []);
+    
     const isLogin = useSelector((state) => {
         if (state.token) return true
         else return false;
     })
 
     return ( 
+        <div>
+        {
+        !userr?(
+            </Loading>
+        ):(
         <div className = "app">
         <Routes>
         <Route path = "/" element = { < LoginPage / > }/> 
@@ -21,6 +59,9 @@ function App() {
         <Route path = "/profile/:userId" element = { isLogin ? < ProfilePage key = { location.pathname } />:<Navigate to="/" />}/> 
         </Routes> 
         </div>
+        )                                             
+        }                                             
+        </div>                                             
         )
     }
 
